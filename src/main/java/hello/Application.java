900ac5c8592bdc6bda4a87f9fa050a584a7b2670
Chih-Sheng <note0972@gmail.com>
2016-12-30 15:53:01 +0800

    2016-12-30 15:52

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Package : hello
+ * --
+ * Description :
+ * Author : jasonlin
+ * Date : 2016/12/30
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,14 @@
+package hello;
+
+/**
+ * Package : hello
+ * --
+ * Description :
+ * Author : jasonlin
+ * Date : 2016/12/30
+ */
+public class Greeter {
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
