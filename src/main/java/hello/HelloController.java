e0941fc08c7253a8856aad728bd2d8bcdacd8d20
Chih-Sheng <note0972@gmail.com>
2017-01-04 11:53:26 +0800

    aaa

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,15 +1,17 @@
 package hello;
 
-import com.linecorp.bot.client.LineMessagingServiceBuilder;
+import com.linecorp.bot.client.LineMessagingService;
 import com.linecorp.bot.model.PushMessage;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.event.*;
+import com.linecorp.bot.model.message.Message;
 import com.linecorp.bot.model.response.BotApiResponse;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
 import com.linecorp.bot.model.message.TextMessage;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
@@ -19,6 +21,9 @@ import lombok.extern.slf4j.Slf4j;
 import retrofit2.Response;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Package : hello
@@ -31,7 +36,8 @@ import java.io.IOException;
 @Slf4j
 @RestController
 public class HelloController {
-    private final String channelAccessToken = "hTjsxjjyaox3OXtKQxydgVHW+txEdRu23nVSJ010ATaXUGNBkTAb+3Z1UaA3q+CiaMqz+YipoXVuSk0vnvBlJcAXAvUCMV/avBCX64Nz1IwiCKosNJhZTaVYKW0ujzaHCCtik7/Bt/zGViS2i+WltwdB04t89/1O/w1cDnyilFU=";
+    @Autowired
+    private LineMessagingService lineMessagingService;
 
     @RequestMapping("/")
     public String index() {
@@ -41,13 +47,39 @@ public class HelloController {
 
     @EventMapping
     public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event){
-        log.debug("Event: " + event);
-        return new TextMessage(event.getMessage().getText());
+        log.info("text event: {}", event);
+        return new TextMessage("I say : " + event.getMessage().getText());
     }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
-        log.debug("Event: " + event);
+        log.info("default event: {}", event);
+    }
+
+    @EventMapping
+    public void handleUnfollowEvent(UnfollowEvent event) {
+        log.info("unfollow event: {}", event);
+    }
+
+    @EventMapping
+    public void handleFollowEvent(FollowEvent event) {
+        log.info("follow event: {}", event);
+        String replyToken = event.getReplyToken();
+        this.replyText(replyToken, "Got followed event : " + event.getSource());
+    }
+
+    @EventMapping
+    public void handleJoinEvent(JoinEvent event) {
+        log.info("join event: {}", event);
+        String replyToken = event.getReplyToken();
+        this.replyText(replyToken, "Joined " + event.getSource());
+    }
+
+    @EventMapping
+    public void handlePostbackEvent(PostbackEvent event) {
+        log.info("postBack event: {}", event);
+        String replyToken = event.getReplyToken();
+        this.replyText(replyToken, "Got postback " + event.getPostbackContent().getData());
     }
 
     @RequestMapping("/greeting")
@@ -61,15 +93,38 @@ public class HelloController {
         Response<BotApiResponse> response =
                 null;
         try {
-            response = LineMessagingServiceBuilder
-                    .create(channelAccessToken)
-                    .build()
-                    .pushMessage(pushMessage)
-                    .execute();
+            response = lineMessagingService
+                        .pushMessage(pushMessage)
+                        .execute();
             return "Success!";
         } catch (IOException e) {
             e.printStackTrace();
         }
         return "Fail!";
+    }
+
+    private void replyText(@NonNull String replyToken, @NonNull String message) {
+        if (replyToken.isEmpty()) {
+            throw new IllegalArgumentException("replyToken must not be empty");
+        }
+        if (message.length() > 1000) {
+            message = message.substring(0, 1000 - 2) + "……";
+        }
+        this.reply(replyToken, new TextMessage(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull Message message) {
+        reply(replyToken, Collections.singletonList(message));
+    }
+
+    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
+        try {
+            Response<BotApiResponse> apiResponse = lineMessagingService
+                    .replyMessage(new ReplyMessage(replyToken, messages))
+                    .execute();
+            log.info("Sent messages: {}", apiResponse);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 }
